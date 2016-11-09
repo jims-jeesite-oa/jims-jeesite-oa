da3513ff43c02403d18647ff912e716a47c90e94
chenxy <15101128707@163.com>
2016-11-09 17:15:18 +0800

    [chenxy]

--- src/main/java/com/thinkgem/jeesite/common/config/Global.java
+++ src/main/java/com/thinkgem/jeesite/common/config/Global.java
@@ -34,7 +34,7 @@ public class Global {
 	/**
 	 * 属性文件加载对象
 	 */
-	private static PropertiesLoader loader = new PropertiesLoader("jeesite.properties");
+	private static PropertiesLoader loader = new PropertiesLoader("jims.properties");
 
 	/**
 	 * 显示/隐藏
