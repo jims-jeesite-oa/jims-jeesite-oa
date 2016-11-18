a8058ba3ecdd77bd60b50de62c0a42b877461e77
chenxy <15101128707@163.com>
2016-11-18 16:15:49 +0800

    [chenxy] todo。

--- todo.sql
+++ todo.sql
@@ -1,3 +1,11 @@
 --todo 登录之后session 不会失效
 --todo 新增用户后，不会将数据同步到act_id_user 与act_id_group
 --todo 项目启动由错误产生
+
+-- todo 编写通用的controller通过传递的参数
+-- todo (表名或者是表单名)不同，依据oa_form_master 查询出 表格表单的样式文件
+-- todo 跳转到通用的jsp页面，将样式文件返回。
+
+--todo 编写通用的jsp(1 将控件绑定到对应的位置   2 对于审批人员能够将申请数据回写出来)
+--todo 封装控件的js代码()
+
