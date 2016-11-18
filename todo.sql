4b6a3cf849d7f0c0d30041f47c0437a88f0ae2d3
chenxy <15101128707@163.com>
2016-11-18 16:44:05 +0800

    [chenxy] maven依赖Poi，自定义表单包。

--- todo.sql
+++ todo.sql
@@ -1,11 +1,19 @@
 --todo 登录之后session 不会失效
 --todo 新增用户后，不会将数据同步到act_id_user 与act_id_group
 --todo 项目启动由错误产生
+--todo 新建的数据库表，核心字段加上索引
 
 -- todo 编写通用的controller通过传递的参数
 -- todo (表名或者是表单名)不同，依据oa_form_master 查询出 表格表单的样式文件
 -- todo 跳转到通用的jsp页面，将样式文件返回。
 
---todo 编写通用的jsp(1 将控件绑定到对应的位置   2 对于审批人员能够将申请数据回写出来)
+--todo 编写通用的jsp(1 将控件绑定到对应的位置(innerhtml)   2 对于审批人员能够将申请数据回写出来)
 --todo 封装控件的js代码()
 
+
+--todo 建立一个通用的控件js文件
+
+
+--todo 加一中生成表单的方式 通过表生成
+--todo 列加一个控件类型
+--todo 遍历字段 创建一个html文件
